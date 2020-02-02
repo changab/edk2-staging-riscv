bac8e0a3a5a9553244be42e57ed73b4e2626cdd7
Daniel Schaefer <git@danielschaefer.me>
2020-02-02 22:46:08 +0100

    Use OpenSBI PPI in DxeIplHandoff

--- RiscVPkg/Library/RiscVDxeIplHandoffOpenSbiLib/RiscVDxeIplHandoffOpenSbiLib.c
+++ RiscVPkg/Library/RiscVDxeIplHandoffOpenSbiLib/RiscVDxeIplHandoffOpenSbiLib.c
@@ -8,15 +8,18 @@
 
 #include <Core/DxeIplPeim/DxeIpl.h>
 #include <IndustryStandard/RiscVOpensbi.h>
-#include <PiPei.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
+#include <Library/PeiServicesLib.h>
+#include <Library/PeiServicesTablePointerLib.h>
+#include <Library/RiscVCpuLib.h>
+#include <Library/RiscVPlatformDxeIpl.h>
+#include <Ppi/RiscVOpenSbiPeim.h>
+#include <PiPei.h>
 #include <sbi/riscv_encoding.h>
 #include <sbi/sbi_hart.h>
 #include <sbi/sbi_init.h>
 #include <sbi/sbi_scratch.h>
-#include <Library/RiscVCpuLib.h>
-#include <Library/RiscVPlatformDxeIpl.h>
 
 /**
    RISC-V platform DXE IPL to DXE OpenSBI mode switch handler.
@@ -74,9 +77,11 @@ RiscVPlatformHandOffToDxeCore (
   IN EFI_PEI_HOB_POINTERS HobList
   )
 {
-  EFI_STATUS Status;
-  struct sbi_scratch *ThisScratch;
-  OPENSBI_SWITCH_MODE_CONTEXT OpenSbiSwitchModeContext;
+  PEI_RISCV_OPENSBI_LIBRARY_PPI  *SbiLib;
+  EFI_STATUS                      Status;
+  struct sbi_scratch             *ThisScratch;
+  OPENSBI_SWITCH_MODE_CONTEXT     OpenSbiSwitchModeContext;
+  EFI_PEI_SERVICES              **PeiServices;
 
   //
   // End of PEI phase signal
@@ -85,6 +90,17 @@ RiscVPlatformHandOffToDxeCore (
   ASSERT_EFI_ERROR (Status);
 
   DEBUG ((DEBUG_INFO, "DXE IPL to DXE Core using OpenSBI\n"));
+
+  PeiServices = (EFI_PEI_SERVICES **) GetPeiServicesTablePointer ();
+
+  Status = PeiServicesLocatePpi (
+             &gPeiOpenSbiLibraryPpiGuid,
+             0,
+             NULL,
+             (VOID **)&SbiLib
+             );
+  ASSERT_EFI_ERROR(Status);
+
   //
   // Setup next address in OpenSBI scratch
   //
@@ -92,17 +108,29 @@ RiscVPlatformHandOffToDxeCore (
   OpenSbiSwitchModeContext.TopOfStack = TopOfStack;
   OpenSbiSwitchModeContext.HobList = HobList;
   OpenSbiSwitchModeContext.DxeCoreEntryPoint = DxeCoreEntryPoint;
-  ThisScratch = sbi_scratch_thishart_ptr ();
+
+  DEBUG ((DEBUG_INFO, "Calling RISC-V OpenSBi PPI: OpenSbiLibraryScratchThisHartPtr\n"));
+  SbiLib->OpenSbiLibraryScratchThisHartPtr (
+            PeiServices,
+            SbiLib,
+            &ThisScratch
+            );
   ThisScratch->next_arg1 = (UINTN)&OpenSbiSwitchModeContext;
   ThisScratch->next_addr = (UINTN)RiscVDxeIplHandoffOpenSbiHandler;
   ThisScratch->next_mode = PRV_S;
 
-  DEBUG ((DEBUG_INFO, "          Base address of satck: 0x%x\n", BaseOfStack));
-  DEBUG ((DEBUG_INFO, "          Top address of satck: 0x%x\n", TopOfStack));
-  DEBUG ((DEBUG_INFO, "          HOB list address: 0x%x\n", &HobList));
-  DEBUG ((DEBUG_INFO, "          DXE core entry pointer: 0x%x\n", DxeCoreEntryPoint));
-  DEBUG ((DEBUG_INFO, "          OpenSBI Switch mode arg1: 0x%x\n", (UINTN)&OpenSbiSwitchModeContext));
+  DEBUG ((DEBUG_INFO, "          Base address of stack:               0x%x\n", BaseOfStack));
+  DEBUG ((DEBUG_INFO, "          Top address of stack:                0x%x\n", TopOfStack));
+  DEBUG ((DEBUG_INFO, "          HOB list address:                    0x%x\n", &HobList));
+  DEBUG ((DEBUG_INFO, "          DXE core entry pointer:              0x%x\n", DxeCoreEntryPoint));
+  DEBUG ((DEBUG_INFO, "          OpenSBI Switch mode arg1:            0x%x\n", (UINTN)&OpenSbiSwitchModeContext));
   DEBUG ((DEBUG_INFO, "          OpenSBI Switch mode handler address: 0x%x\n", (UINTN)RiscVDxeIplHandoffOpenSbiHandler));
-  DEBUG ((DEBUG_INFO, "          OpenSBI Switch mode to privilege 0x%x\n", PRV_S));
-  sbi_init (ThisScratch);
+  DEBUG ((DEBUG_INFO, "          OpenSBI Switch mode to privilege:    0x%x\n", PRV_S));
+
+  DEBUG ((DEBUG_INFO, "Calling RISC-V OpenSBi PPI: OpenSbiLibrarySbiInit\n"));
+  SbiLib->OpenSbiLibrarySbiInit (
+            PeiServices,
+            SbiLib,
+            ThisScratch
+            );
 }
